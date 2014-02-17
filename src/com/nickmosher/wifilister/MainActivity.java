a5e891e5f160e2ef490929f17b0bcc5a300ed1fe
Nick Mosher <codewhisperer97@gmail.com>
2014-02-17 13:05:09 -0500

    Using LayoutInflater rather than plain TextView

--- src/com/nickmosher/wifilister/MainActivity.java
+++ src/com/nickmosher/wifilister/MainActivity.java
@@ -8,13 +8,16 @@ import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
 	private Context context;
+	private LayoutInflater inflater;
 	private WifiManager wifiManager;
 	private List<WifiConfiguration>networksList;
 	
@@ -26,18 +29,33 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         
         context = this;
+        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
         networksList = wifiManager.getConfiguredNetworks();
         
         linearLayout0 = (LinearLayout)findViewById(R.id.linearLayout0);
         
+        View aView;
+        
+        /*
+         * Loop that generates 10 View items to try to fill up some of the scrolling space
+         */
+        for(int i = 0; i < 10; i++) {
+        	aView = inflater.inflate(R.layout.list_item, null, false);
+        	((TextView) aView).setText("Filler TextView");
+        	linearLayout0.addView(aView);
+        }
+        
+        /*
+         * Retrieves the names for configured SSIDs and sets them for the text of TextViews
+         */
         Iterator<WifiConfiguration> iterator = networksList.iterator();
         WifiConfiguration config;
         while(iterator.hasNext()) {
         	config = (WifiConfiguration) iterator.next();
-        	TextView textView = new TextView(this);
-        	textView.setText(config.SSID);
-        	linearLayout0.addView(textView);
+        	aView = inflater.inflate(R.layout.list_item, null, false);
+        	((TextView) aView).setText(config.SSID);
+        	linearLayout0.addView(aView);
         }
     }
 
